407bcb3cb2182b8075fff68da0a5f642ad447478
Madelyn Nelson <notamusicapp@gmail.com>
2022-12-27 20:35:04 -0500

    adding registry page, currently only has marathon fundraising links

--- header.php
+++ header.php
@@ -2,56 +2,58 @@
     <head>
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>  
       <meta charset="UTF-8">
-        <!-- Compiled and minified CSS -->
-         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<!-- Compiled and minified CSS -->
+	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
-        <!-- Compiled and minified JavaScript -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    
-        <!--Import Google Icon Font-->
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
-        <!--Import materialize.css
-        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>-->
+	<!-- Compiled and minified JavaScript -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+	<!--Import Google Icon Font-->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+	<!--Import materialize.css
+	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>-->
 
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link rel="stylesheet" href="assets/stylesheets/main.css">
+	<link rel="stylesheet" href="assets/stylesheets/main.css">
 
-        <link rel="stylesheet" media="screen" href="https://fontlibrary.org//face/cursive" type="text/css"/> 
-        <link rel="stylesheet" media="screen" href="https://fontlibrary.org//face/metropolitano" type="text/css"/> 
-        <title>Madelyn and Timothy - Welcome!</title>
-        <script src="scripts.js"></script>
+	<link rel="stylesheet" media="screen" href="https://fontlibrary.org//face/cursive" type="text/css"/> 
+	<link rel="stylesheet" media="screen" href="https://fontlibrary.org//face/metropolitano" type="text/css"/> 
+	<title>Madelyn and Timothy - Welcome!</title>
+	<script src="scripts.js"></script>
       </head>
 
     <body>
       <header>
       <ul id="dropdownstory" class="dropdown-content">
-        <li><a href="#!">Bride and Groom</a></li>
-        <li><a href="#!">Bridal Party</a></li>
-        <li class="divider"></li>
-        <li><a href="#!">Family</a></li>
+	<li><a href="#!">Bride and Groom</a></li>
+	<li><a href="#!">Bridal Party</a></li>
+	<li class="divider"></li>
+	<li><a href="#!">Family</a></li>
       </ul>
       <ul id="dropdownlogistics" class="dropdown-content">
-        <li><a href="logistics.php">Arrival and Lodging</a></li>
-        <li><a href="#!">Ceremony and Reception</a></li>
-        <li class="divider"></li>
-        <li><a href="#!">Things to Do</a></li>
+	<li><a href="logistics.php">Arrival and Lodging</a></li>
+	<li><a href="#!">Ceremony and Reception</a></li>
+	<li class="divider"></li>
+	<li><a href="#!">Things to Do</a></li>
       </ul>
       <div class="navbar-fixed">
       <nav class="nav-extended">
-        <div class = "teal darken-4 nav-wrapper">
-          <a href = "index.php" class = "brand-logo center">Madelyn & Timothy</a>
-          <ul id = "nav-mobile" class="left hide-on-med-and-down">
-            <li><a href="weddingParty.php">Our Story</a></li>
-            <li><a class="dropdown-trigger" href="#!" data-target="dropdownlogistics">Logistics<i class="material-icons right">arrow_drop_down</i></a></li>
-            <!--<li><a href="#">Logistics</a></li>-->
-          </ul>
-          <ul id = "nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="#">RSVP</a></li>
-            <li><a href="#">Registry</a></li>
-          </ul>
-        </div>
+	<div class = "teal darken-4 nav-wrapper">
+	  <a href = "index.php" class = "brand-logo center">Madelyn & Timothy</a>
+	  <ul id = "nav-mobile" class="left hide-on-med-and-down">
+	    <li><a href="weddingParty.php">Our Story</a></li>
+	    <li><a class="dropdown-trigger" href="#!" data-target="dropdownlogistics">Logistics<i class="material-icons right">arrow_drop_down</i></a></li>
+	    <!--<li><a href="#">Logistics</a></li>-->
+	  </ul>
+	  <ul id = "nav-mobile" class="right hide-on-med-and-down">
+	    <li><a href="#">RSVP</a></li>
+	    <li><a href="registry.php">Registry</a></li>
+	  </ul>
+	</div>
       </nav>
       </div>
       </header>
-       
+</body>
+</html>
+
---
+++ registry.php
@@ -0,0 +1,50 @@
+<?php
+#$title = "Php Header Footer";                   
+require_once "header.php";              
+?>
+<style><?php include 'assets/stylesheets/main.css'; ?></style>
+
+  <main>
+
+    <div class="section">
+      <div class="row center">
+	<div class="col s8 offset-s2">
+
+	  <h3 class="grey-text text-darken-3">We're still working on our registry</h3>
+
+	  <p>In the meantime, if you'd like to contribute to our 2023 Chicago Marathon fundraising for <a href="https://www.missionola.com/">Mission of Our Lady of the Angels food pantry</a>, please feel free :)</p>
+
+      	</div>
+      </div>
+      <div class="row center">
+
+	<div class="col s8 offset-s2 m4 offset-m2">
+	<div class="card">
+	    <div class="card-image waves-effect waves-block waves-light">
+		<img class="activator" src="assets/images/marathon/madelyn_marathon_2023.jpeg">
+	    </div>
+	    <div class="card-content">
+		<span class="card-title activator grey-text text-darken-4">Madelyn's Chicago Marathon Fundraising Page</span>
+		<p><a href="https://secure.qgiv.com/event/teamola2023/account/1401069/">Mission of Our Lady of the Angels</a></p>
+	    </div>
+	</div>
+	</div>
+
+	<div class="col s8 offset-s2 m4">
+	<div class="card">
+	    <div class="card-image waves-effect waves-block waves-light">
+		<img class="activator" src="assets/images/marathon/tim_marathon_2023.jpeg">
+	    </div>
+	    <div class="card-content">
+		<span class="card-title activator grey-text text-darken-4">Tim's Chicago Marathon Fundraising Page</span>
+		<p><a href="https://secure.qgiv.com/event/teamola2023/account/1401632/">Mission of Our Lady of the Angels</a></p>
+	    </div>
+	</div>
+	</div>
+
+      </div>
+    </div>
+
+<?php
+require_once "footer.php";                
+?>
