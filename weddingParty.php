8836204eed05b08b18c8cb223766414e7d27fda4
Madelyn Nelson <notamusicapp@gmail.com>
2022-12-27 21:15:04 -0500

    adding formatting and lorem ipsum to weddingParty

--- weddingParty.php
+++ weddingParty.php
@@ -28,44 +28,45 @@ left side blurb, right side photo
     </div>
 -->
 
+    <div class="section">
+        <div class="row center">
+	    <br/>
+            <h5>Learn a little about our bridal party</h5>
+	</div>
+    </div>
+
+    <div class="section">
+        <div class="row center">
+
+	    <div class="col s5 offset-s1">
+		<div class="row center">
+		  <div class="col s10 offset-s1">
+		    <img style="border-radius: 50%;" src="https://picsum.photos/200"/>
+                    <h3>Madelyn</h3>
+		    <h5 class="bridal-party-role">BRIDE</h5>
+		    <p>
+		    Toad-like smile Flourish and Blotts he knew I’d come back Quidditch World Cup. Fat Lady baubles banana fritters fairy lights Petrificus Totalus. So thirsty, deluminator firs’ years follow me 12 inches of parchment. Head Boy start-of-term banquet Cleansweep Seven roaring lion hat. Unicorn blood crossbow mars is bright tonight, feast Norwegian Ridgeback. Come seek us where our voices sound, we cannot sing above the ground, Ginny Weasley bright red. Fanged frisbees, phoenix tears good clean match.
+                   </p>
+                </div>
+              </div>
+            </div>
+
+	    <div class="col s5" style="border-left: 1px solid black;">
+		<div class="row center">
+		  <div class="col s10 offset-s1">
+		    <img style="border-radius: 50%;" src="https://picsum.photos/200"/>
+                    <h3>Timothy</h3>
+		    <h5 class="bridal-party-role">GROOM</h5>
+		    <p>
+                   Thestral dirigible plums, Viktor Krum hexed memory charm Animagus Invisibility Cloak three-headed Dog. Half-Blood Prince Invisibility Cloak cauldron cakes, hiya Harry! Basilisk venom Umbridge swiveling blue eye Levicorpus, nitwit blubber oddment tweak. Chasers Winky quills The Boy Who Lived bat spleens cupboard under the stairs flying motorcycle. Sirius Black Holyhead Harpies, you’ve got dirt on your nose. Floating candles Sir Cadogan The Sight three hoops disciplinary hearing. Grindlewald pig’s tail Sorcerer's Stone biting teacup. Side-along dragon-scale suits Filch 20 points, Mr. Potter.</p>
+                </div>
+              </div>
+            </div>
+
+        </div>
+    </div>
+
     
-
-
-    <div class="row deep-purple">
-      <div class="col s6 m6 l6">
-        <div class="card">
-        <div class="card-image">
-          <img src="assets/images/weddingParty/headshot.jpg">
-        </div>
-      </div>
-      </div>
-      
-      <div class="col s6">
-        <div class="card">
-          <div class="card-content teal darken-4">
-            <p class="center-align flow-text">This is some crap about Tim</p>
-          </div>
-        </div>
-      </div>
-    </div> 
-
-    <div class="row teal">
-      <div class="col s6 m6 l6">
-        <div class="card">
-        <div class="card-content teal darken-4">
-            <p class="center-align flow-text">This is some crap about Madelyn</p>
-          </div>
-      </div>
-      </div>
-      
-      <div class="col s6">
-        <div class="card">
-        <div class="card-image">
-          <img src="assets/images/weddingParty/madelynRedDress.jpg">
-        </div>
-        </div>
-      </div>
-    </div> 
      
 <?php
 require_once "footer.php";                
