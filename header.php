d8b375091391e588626ad6da4b8aaeab571ea7cd
Madelyn Nelson <notamusicapp@gmail.com>
2023-01-03 22:08:56 -0600

    fixed responsiveness, moved <head> to each page so we can more granular analytics data

--- header.php
+++ header.php
@@ -1,38 +1,4 @@
 <html>
-    <head>
-	<!-- Google tag (gtag.js) -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-7PPTGD0FHQ"></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag('js', new Date());
-
-		gtag('config', 'G-7PPTGD0FHQ');
-	</script>
-
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>  
-      <meta charset="UTF-8">
-	<!-- Compiled and minified CSS -->
-	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-
-	<!-- Compiled and minified JavaScript -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-
-	<!--Import Google Icon Font-->
-	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
-	<!--Import materialize.css
-	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>-->
-
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	<link rel="stylesheet" href="assets/stylesheets/main.css">
-
-	<link rel="stylesheet" media="screen" href="https://fontlibrary.org//face/cursive" type="text/css"/> 
-	<link rel="stylesheet" media="screen" href="https://fontlibrary.org//face/metropolitano" type="text/css"/> 
-	<title>Madelyn and Timothy - Welcome!</title>
-	<script src="scripts.js"></script>
-      </head>
-
     <body>
       <header>
       <ul id="dropdownstory" class="dropdown-content">
@@ -47,22 +13,25 @@
 	<li class="divider"></li>
 	<li><a href="#!">Things to Do</a></li>
       </ul>
-      <div class="navbar-fixed">
-      <nav class="nav-extended">
-	<div class = "teal darken-4 nav-wrapper">
-	  <a href = "index.php" class = "brand-logo center">Madelyn & Timothy</a>
-	  <ul id = "nav-mobile" class="left hide-on-med-and-down">
+
+<!--      <div class="navbar-fixed"> -->
+      <nav class="side-nav">
+	<div class="teal darken-4 nav-wrapper">
+	  <a href="index.php" class="brand-logo center hide-on-small-only">Madelyn & Timothy</a>
+	  <a href="index.php" class="brand-logo center hide-on-med-and-up show-on-small">M & T</a>
+	  <ul id="nav-mobile" class="left">
 	    <li><a href="weddingParty.php">Our Story</a></li>
-	    <li><a class="dropdown-trigger" href="#!" data-target="dropdownlogistics">Logistics<i class="material-icons right">arrow_drop_down</i></a></li>
-	    <!--<li><a href="#">Logistics</a></li>-->
+	    <!--<li><a class="dropdown-trigger" href="#!" data-target="dropdownlogistics">Logistics<i class="material-icons right">arrow_drop_down</i></a></li>-->
+	    <li><a href="#">Logistics</a></li>
 	  </ul>
-	  <ul id = "nav-mobile" class="right hide-on-med-and-down">
+	  <ul id="nav-mobile" class="right">
 	    <li><a href="rsvp.php">RSVP</a></li>
 	    <li><a href="registry.php">Registry</a></li>
 	  </ul>
 	</div>
       </nav>
-      </div>
+      <!--</div>-->
+
       </header>
 </body>
 </html>
--- weddingParty.php
+++ weddingParty.php
@@ -1,3 +1,38 @@
+<html>
+    <head>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-7PPTGD0FHQ"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-7PPTGD0FHQ');
+	</script>
+
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>  
+      <meta charset="UTF-8">
+	<!-- Compiled and minified CSS -->
+	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+	<!-- Compiled and minified JavaScript -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+	<!--Import Google Icon Font-->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+	<!--Import materialize.css
+	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>-->
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<link rel="stylesheet" href="assets/stylesheets/main.css">
+
+	<link rel="stylesheet" media="screen" href="https://fontlibrary.org//face/cursive" type="text/css"/> 
+	<link rel="stylesheet" media="screen" href="https://fontlibrary.org//face/metropolitano" type="text/css"/> 
+	<title>Madelyn and Timothy - Welcome!</title>
+	<script src="scripts.js"></script>
+      </head>
+
 <?php
 #$title = "Php Header Footer";                   
 require_once "header.php";              
@@ -30,20 +65,20 @@ left side blurb, right side photo
     <div class="section">
         <div class="row center">
 
-	    <div class="col s5 offset-s1">
+	    <div class="col m5 offset-m1 s10 offset-s1">
 		<div class="row center">
 		  <div class="col s10 offset-s1">
-		    <img style="border-radius: 50%;" src="https://picsum.photos/200"/>
+		    <img style="border-radius: 50%;" src="/assets/images/bridal_party/madelyn_red_dress.jpeg" height=200>
                     <h3>Madelyn</h3>
 		    <h5 class="teal-text text-darken-4 bridal-party-role">Bride</h5>
 		    <p>
-		    Toad-like smile Flourish and Blotts he knew I’d come back Quidditch World Cup. Fat Lady baubles banana fritters fairy lights Petrificus Totalus. So thirsty, deluminator firs’ years follow me 12 inches of parchment. Head Boy start-of-term banquet Cleansweep Seven roaring lion hat. Unicorn blood crossbow mars is bright tonight, feast Norwegian Ridgeback. Come seek us where our voices sound, we cannot sing above the ground, Ginny Weasley bright red. Fanged frisbees, phoenix tears good clean match.
+			Madelyn is a Notre Dame-loving, plant-growing, lemon-rice-soup-cooking young woman, and she is absolutely delighted to be marrying Tim! She works remotely as a backend software engineer, and leads the Safe Families for Children ministry at St. Alphonsus Church. Her favorite activities include biking around the city with Tim, maintaining a small city vegetable garden, and trying new foods (as long as they're not too spicy). She has two adorable cats, Obie and Kareem, and has successfully taught them a few tricks! 
                    </p>
                 </div>
               </div>
             </div>
 
-	    <div class="col s5" style="border-left: 1px solid black;">
+	    <div class="col m5 s10 offset-s1" style="border-left: 1px solid black;">
 		<div class="row center">
 		  <div class="col s10 offset-s1">
 		    <img style="border-radius: 50%;" src="https://picsum.photos/200"/>
@@ -66,20 +101,20 @@ left side blurb, right side photo
         </div>
         <div class="row center">
 
-	    <div class="col s5 offset-s1">
+	    <div class="col m5 offset-m1 s10 offset-s1">
 		<div class="row center">
 		  <div class="col s10 offset-s1">
-		    <img style="border-radius: 50%;" src="https://picsum.photos/200"/>
+		    <img style="border-radius: 50%;" src="/assets/images/bridal_party/alex_gray_sweater.jpeg" height=200>
                     <h3>Alexandria</h3>
 		    <h5 class="teal-text text-darken-4 bridal-party-role">Maid of Honor</h5>
 		    <p>
-		    Toad-like smile Flourish and Blotts he knew I’d come back Quidditch World Cup. Fat Lady baubles banana fritters fairy lights Petrificus Totalus. So thirsty, deluminator firs’ years follow me 12 inches of parchment. Head Boy start-of-term banquet Cleansweep Seven roaring lion hat. Unicorn blood crossbow mars is bright tonight, feast Norwegian Ridgeback. Come seek us where our voices sound, we cannot sing above the ground, Ginny Weasley bright red. Fanged frisbees, phoenix tears good clean match.
+                   Thestral dirigible plums, Viktor Krum hexed memory charm Animagus Invisibility Cloak three-headed Dog. Half-Blood Prince Invisibility Cloak cauldron cakes, hiya Harry! Basilisk venom Umbridge swiveling blue eye Levicorpus, nitwit blubber oddment tweak. Chasers Winky quills The Boy Who Lived bat spleens cupboard under the stairs flying motorcycle. Sirius Black Holyhead Harpies, you’ve got dirt on your nose. Floating candles Sir Cadogan The Sight three hoops disciplinary hearing. Grindlewald pig’s tail Sorcerer's Stone biting teacup. Side-along dragon-scale suits Filch 20 points, Mr. Potter.</p>
                    </p>
                 </div>
               </div>
             </div>
 
-	    <div class="col s5" style="border-left: 1px solid black;">
+	    <div class="col m5 s10 offset-s1" style="border-left: 1px solid black;">
 		<div class="row center">
 		  <div class="col s10 offset-s1">
 		    <img style="border-radius: 50%;" src="https://picsum.photos/200"/>
@@ -91,20 +126,20 @@ left side blurb, right side photo
               </div>
             </div>
 
-	    <div class="col s5 offset-s1">
+	    <div class="col m5 offset-m1 s10 offset-s1">
 		<div class="row center">
 		  <div class="col s10 offset-s1">
-		    <img style="border-radius: 50%;" src="https://picsum.photos/200"/>
+		    <img style="border-radius: 50%;" src="/assets/images/bridal_party/nicole_chicago_river.jpeg" height=200>
                     <h3>Nicole</h3>
 		    <h5 class="teal-text text-darken-4 bridal-party-role">Bridesmaid</h5>
 		    <p>
-		    Toad-like smile Flourish and Blotts he knew I’d come back Quidditch World Cup. Fat Lady baubles banana fritters fairy lights Petrificus Totalus. So thirsty, deluminator firs’ years follow me 12 inches of parchment. Head Boy start-of-term banquet Cleansweep Seven roaring lion hat. Unicorn blood crossbow mars is bright tonight, feast Norwegian Ridgeback. Come seek us where our voices sound, we cannot sing above the ground, Ginny Weasley bright red. Fanged frisbees, phoenix tears good clean match.
-                   </p>
+                   Thestral dirigible plums, Viktor Krum hexed memory charm Animagus Invisibility Cloak three-headed Dog. Half-Blood Prince Invisibility Cloak cauldron cakes, hiya Harry! Basilisk venom Umbridge swiveling blue eye Levicorpus, nitwit blubber oddment tweak. Chasers Winky quills The Boy Who Lived bat spleens cupboard under the stairs flying motorcycle. Sirius Black Holyhead Harpies, you’ve got dirt on your nose. Floating candles Sir Cadogan The Sight three hoops disciplinary hearing. Grindlewald pig’s tail Sorcerer's Stone biting teacup. Side-along dragon-scale suits Filch 20 points, Mr. Potter.</p>
+		   </p>
                 </div>
               </div>
             </div>
 
-	    <div class="col s5" style="border-left: 1px solid black;">
+	    <div class="col m5 s10 offset-s1" style="border-left: 1px solid black;">
 		<div class="row center">
 		  <div class="col s10 offset-s1">
 		    <img style="border-radius: 50%;" src="https://picsum.photos/200"/>
@@ -116,20 +151,20 @@ left side blurb, right side photo
               </div>
             </div>
 
-	    <div class="col s5 offset-s1">
+	    <div class="col m5 offset-m1 s10 offset-s1">
 		<div class="row center">
 		  <div class="col s10 offset-s1">
-		    <img style="border-radius: 50%;" src="https://picsum.photos/200"/>
+		    <img style="border-radius: 50%;" src="/assets/images/bridal_party/erica_pink_top.jpeg" height=200>
                     <h3>Erica</h3>
 		    <h5 class="teal-text text-darken-4 bridal-party-role">Bridesmaid</h5>
 		    <p>
-		    Toad-like smile Flourish and Blotts he knew I’d come back Quidditch World Cup. Fat Lady baubles banana fritters fairy lights Petrificus Totalus. So thirsty, deluminator firs’ years follow me 12 inches of parchment. Head Boy start-of-term banquet Cleansweep Seven roaring lion hat. Unicorn blood crossbow mars is bright tonight, feast Norwegian Ridgeback. Come seek us where our voices sound, we cannot sing above the ground, Ginny Weasley bright red. Fanged frisbees, phoenix tears good clean match.
+                   Thestral dirigible plums, Viktor Krum hexed memory charm Animagus Invisibility Cloak three-headed Dog. Half-Blood Prince Invisibility Cloak cauldron cakes, hiya Harry! Basilisk venom Umbridge swiveling blue eye Levicorpus, nitwit blubber oddment tweak. Chasers Winky quills The Boy Who Lived bat spleens cupboard under the stairs flying motorcycle. Sirius Black Holyhead Harpies, you’ve got dirt on your nose. Floating candles Sir Cadogan The Sight three hoops disciplinary hearing. Grindlewald pig’s tail Sorcerer's Stone biting teacup. Side-along dragon-scale suits Filch 20 points, Mr. Potter.</p>
                    </p>
                 </div>
               </div>
             </div>
 
-	    <div class="col s5" style="border-left: 1px solid black;">
+	    <div class="col m5 s10 offset-s1" style="border-left: 1px solid black;">
 		<div class="row center">
 		  <div class="col s10 offset-s1">
 		    <img style="border-radius: 50%;" src="https://picsum.photos/200"/>
@@ -149,3 +184,5 @@ left side blurb, right side photo
 <?php
 require_once "footer.php";                
 ?>
+
+</html>
