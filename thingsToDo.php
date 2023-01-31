160b8cd1ee47c5ffbb43da9ad79a6d40d67fde81
Madelyn Nelson <notamusicapp@gmail.com>
2023-01-30 23:50:26 -0600

    updated thingsToDo page

--- thingsToDo.php
+++ thingsToDo.php
@@ -16,12 +16,12 @@ require_once "header.php";
 	<div class="row center">
 		<div class="col m8 offset-m2 s10 offset-s1">
 
-			<h2 class="grey-text text-darken-3">City Attractions</h2>
+			<h3 class="grey-text text-darken-3">City Attractions</h3>
 
-			<p>Chicago is a world-class city with many, <i>many</i>, possible ways to spend your time. Athletics and leisure, fine dining and local dives, historical institutions and innovative , </p>
+			<p>Chicago is a world-class city with many, <i>many</i>, possible ways to spend your time. Athletics and leisure, fine dining and local dives, historical institutions and innovative TODO, TODO</p>
 
-			https://freetoursbyfoot.com/chicago-tours/book-chicago-tours/
-			https://www.lpzoo.org/visit/
+			<a href="https://freetoursbyfoot.com/chicago-tours/book-chicago-tours/">https://freetoursbyfoot.com/chicago-tours/book-chicago-tours/</a>
+			<a href="https://www.lpzoo.org/visit/">https://www.lpzoo.org/visit/</a>
 
 
 		</div>
@@ -29,6 +29,10 @@ require_once "header.php";
 
 	<div class="row center">
 		<image height=150 src="/assets/images/flowers/purple_flowers_long_2.png" />
+	</div>
+
+	<div class="row center">
+		<h3 class="grey-text text-darken-3">In Our Neighborhood</h3>
 	</div>
 
 	<div class="row center">
@@ -43,11 +47,11 @@ require_once "header.php";
 				</div>
 				<div class="col m6 s12">
 					<h5 class="teal-text text-darken-4 wedding-date"><b>Intelligentsia Coffee - Broadway Coffeebar</b></h5>
-					<p>First Date Location</p>
+					<p>First Date Location - Part 1</p>
 					<a href="https://goo.gl/maps/D7Qu8XXFENWpAS3n8">
 						<p>3123 N Broadway, Chicago, IL 60657</p>
 					</a>
-					<br/>
+					<br />
 					<p>
 						We highly recommend our favorite local coffee shop, Intelligentsia! Not only is it where we first met #HingeDate, it's also continues to be our favorite source of local coffee grounds. Strong believers in DEI & ethically-sourced coffee.
 					</p>
@@ -61,35 +65,35 @@ require_once "header.php";
 					<h5 class="teal-text text-darken-4 wedding-date"><b>Diversey Harbor (Chevron Sculpture)</b></h5>
 					<p>Proposal Location</p>
 					<a href="https://goo.gl/maps/P3rppeLECaBVdxXD7">
-						<p>(Approx.) 141 W Diversey Pkwy, Chicago, IL 60657</p>
+						<p>Diversey & Lake Michigan</p>
 					</a>
-					<br/>
+					<br />
 					<p>
 						Known to locals as "that blue windmill sculpture by the lake", this was one of the places we walked by on our first date... and the beautiful place Tim proposed just over a year later! Great view of the city skyline and Lake Michigan, and directly next to the beautiful 18.5-mile lakefront path. To access, cross the sidewalk under Lake Shore Drive toward the lake.
 					</p>
 				</div>
 				<div class="col m6 s12">
-					<a href="https://www.stalphonsuschicago.org/">
+					<a href="">
 						<img src="assets/images/things_to_do/diversey_harbor.jpeg" width="100%" style="border-radius:20px"></img>
 					</a>
 				</div>
 			</div>
 
-			<br/>
+			<br />
 
 			<div class="row">
 				<div class="col m6 s12">
-					<a href="https://www.intelligentsia.com/pages/broadway-coffeebar">
+					<a href="https://www.veggiediner.com/?y_source=1_MjY4OTc1NzgtNzE1LWxvY2F0aW9uLndlYnNpdGU%3D">
 						<img src="assets/images/things_to_do/the_chicago_diner.jpeg" width="100%" style="border-radius:20px"></img>
 					</a>
 				</div>
 				<div class="col m6 s12">
 					<h5 class="teal-text text-darken-4 wedding-date"><b>The Chicago Diner</b></h5>
-					<p>Second Date Location</p>
-					<a href="https://goo.gl/maps/D7Qu8XXFENWpAS3n8">
-						<p>3123 N Broadway, Chicago, IL 60657</p>
+					<p>Second Date Location - Part 1</p>
+					<a href="https://goo.gl/maps/h6Z22xKvtQJbTgB69">
+						<p>3411 N Halsted, Chicago, IL 60657</p>
 					</a>
-					<br/>
+					<br />
 					<p>
 						When our first two lunch date location ideas were unexpectedly closed, we decided on plan C (the only restaurant open for a mid-week late lunch) and ended up loving it. Chicago-themed vegetarian & vegan restaurant in the heart of North Halsted with plenty of PBR. Consistently one of the highest-ranked vegetarian & vegan establishments in the city.
 					</p>
@@ -98,9 +102,250 @@ require_once "header.php";
 
 			<br />
 
+			<div class="row">
+				<div class="col m6 s12">
+					<h5 class="teal-text text-darken-4 wedding-date"><b>Bill Jarvis Migratory Bird Sanctuary</b></h5>
+					<p>First Date Location - Part 2</p>
+					<a href="https://goo.gl/maps/4CZ3p52AJptCQqTC8">
+						<p>3550 N Lake Shore Dr. Chicago, IL 60657</p>
+					</a>
+					<br />
+					<p>
+						After stopping by Intelligentsia for coffee on our first date, we excitedly walked along the lakefront and ended up strolling through the Bill Jarvis bird sanctuary. It's a great nature oasis nestled between highrises and Lake Michigan - perfect for a peaceful 15-minute stroll. To access, cross under Lake Shore Drive at Addison.
+					</p>
+				</div>
+				<div class="col m6 s12">
+					<a href="">
+						<img src="assets/images/things_to_do/bill_jarvis_bird_sanctuary.jpeg" width="100%" style="border-radius:20px"></img>
+					</a>
+				</div>
+			</div>
+
+			<br />
+
+			<div class="row">
+				<div class="col m6 s12">
+					<a href="http://www.unabridgedbookstore.com/">
+						<img src="assets/images/things_to_do/unabridged_bookstore.jpeg" width="100%" style="border-radius:20px"></img>
+					</a>
+				</div>
+				<div class="col m6 s12">
+					<h5 class="teal-text text-darken-4 wedding-date"><b>Unabridged Bookstore</b></h5>
+					<p>Favorite Local Bookstore</p>
+					<a href="https://goo.gl/maps/pBhdamNh1V4KPGnx5">
+						<p>3251 N Broadway, Chicago, IL 60657</p>
+					</a>
+					<br />
+					<p>
+						Yay books! So many books!
+					</p>
+				</div>
+			</div>
+
+			<br />
+
+			<div class="row">
+				<div class="col m6 s12">
+					<h5 class="teal-text text-darken-4 wedding-date"><b>Panes Bread Cafe</b></h5>
+					<p>Favorite Local Restaurant</p>
+					<a href="https://goo.gl/maps/RBLgXHQB3Q6Dofng6">
+						<p>3002 N Sheffield Ave, Chicago, IL 60657</p>
+					</a>
+					<br />
+					<p>
+						Panes has been our favorite go-to place since we started dating. They're known for amazing homemade bread, sandwiches, and desserts.
+					</p>
+				</div>
+				<div class="col m6 s12">
+					<a href="http://places.singleplatform.com/panes-bread-cafe/menu?ref=google">
+						<img src="assets/images/things_to_do/panes_bread_cafe.jpeg" width="100%" style="border-radius:20px"></img>
+					</a>
+				</div>
+			</div>
+
+			<br />
+
+			<div class="row">
+				<div class="col m6 s12">
+					<a href="http://www.unabridgedbookstore.com/">
+						<img src="assets/images/things_to_do/sumo.png" width="100%" style="border-radius:20px"></img>
+					</a>
+				</div>
+				<div class="col m6 s12">
+					<h5 class="teal-text text-darken-4 wedding-date"><b>Sumo Sushi & Hibachi</b></h5>
+					<p>Post-Proposal Dinner Location</p>
+					<a href="https://goo.gl/maps/obZZcaXThTvAhk6H8">
+						<p>2936 N Broadway #5302, Chicago, IL 60657</p>
+					</a>
+					<br />
+					<p>
+						We love SUSHI!
+					</p>
+				</div>
+			</div>
+
+			<br />
+
+			<div class="row">
+				<div class="col m6 s12">
+					<h5 class="teal-text text-darken-4 wedding-date"><b>The Four Treys Tavern</b></h5>
+					<p>Second Date Location - Part 2</p>
+					<a href="https://goo.gl/maps/ugW5XRt4TJKE1iG48">
+						<p>3333 N Damen Ave, Chicago, IL 60618</p>
+					</a>
+					<br />
+					<p>
+						Yup it's a dive bar. This is where Tim's legs started shaking, so he finally asked if we could sit down to talk instead of walking another 4 miles.
+					</p>
+				</div>
+				<div class="col m6 s12">
+					<a href="">
+						<img src="assets/images/things_to_do/the_four_treys_tavern.png" width="100%" style="border-radius:20px"></img>
+					</a>
+				</div>
+			</div>
+
+			<br />
+
+		</div>
+
+		<div class="row center">
+			<image height=150 src="/assets/images/flowers/purple_flowers_long_2.png" />
+		</div>
+
+		<div class="row center">
+			<h3 class="grey-text text-darken-3">A Little Farther Away (but still close!)</h3>
+		</div>
+
+		<div class="row center">
+			<div class="col m8 offset-m2 s10 offset-s1">
+
+
+				<div class="row">
+					<div class="col m6 s12">
+						<a href="https://www.lpzoo.org/">
+							<img src="assets/images/things_to_do/lincoln_park_zoo.webp" width="100%" style="border-radius:20px"></img>
+						</a>
+					</div>
+					<div class="col m6 s12">
+						<h5 class="teal-text text-darken-4 wedding-date"><b>Lincoln Park Zoo</b></h5>
+						<a href="https://goo.gl/maps/exayCQ2UWYi5kQWo9">
+							<p>2001 N Clark St, Chicago, IL 60614</p>
+						</a>
+						<br />
+						<p>
+							Lions and tigers and bears OH MY
+						</p>
+					</div>
+				</div>
+
+				<br />
+
+				<div class="row">
+					<div class="col m6 s12">
+						<h5 class="teal-text text-darken-4 wedding-date"><b>Wrigley Field</b></h5>
+						<p>Home of the Chicago Cubs</p>
+						<a href="https://goo.gl/maps/YuKttGThRUWutrcN6">
+							<p>1060 W Addison St, Chicago, IL 60613</p>
+						</a>
+						<br />
+						<p>
+							Go Cubs Go!
+						</p>
+					</div>
+					<div class="col m6 s12">
+						<a href="http://chicago.cubs.mlb.com/chc/ballpark/">
+							<img src="assets/images/things_to_do/wrigley_field.jpeg" width="100%" style="border-radius:20px"></img>
+						</a>
+					</div>
+				</div>
+
+				<br />
+
+				<div class="row">
+					<div class="col m6 s12">
+						<a href="http://www.holynamecathedral.org/">
+							<img src="assets/images/things_to_do/holy_name_cathedral.webp" width="100%" style="border-radius:20px"></img>
+						</a>
+					</div>
+					<div class="col m6 s12">
+						<h5 class="teal-text text-darken-4 wedding-date"><b>Holy Name Cathedral</b></h5>
+						<a href="https://goo.gl/maps/HNzumbCdCJ4UiFRL6">
+							<p>735 N State St, Chicago, IL 60654</p>
+						</a>
+						<br />
+						<p>
+							Big old Cathedral so cool!
+						</p>
+					</div>
+				</div>
+
+				<br />
+
+				<div class="row">
+					<div class="col m6 s12">
+						<h5 class="teal-text text-darken-4 wedding-date"><b>Lincoln Park Conservatory</b></h5>
+						<a href="https://goo.gl/maps/7FmgB3Xa272dcahq8">
+							<p>2391 N Stockton Dr, Chicago, IL 60614</p>
+						</a>
+						<br />
+						<p>
+							Lots of plants
+						</p>
+					</div>
+					<div class="col m6 s12">
+						<a href="http://lincolnparkconservancy.org/projects/conservatory-garden/">
+							<img src="assets/images/things_to_do/lincoln-park-conservatory.jpeg" width="100%" style="border-radius:20px"></img>
+						</a>
+					</div>
+				</div>
+
+				<br />
+
+				<div class="row">
+					<div class="col m6 s12">
+						<a href="">
+							<img src="assets/images/things_to_do/north_avenue_beach.jpeg" width="100%" style="border-radius:20px"></img>
+						</a>
+					</div>
+					<div class="col m6 s12">
+						<h5 class="teal-text text-darken-4 wedding-date"><b>North Avenue Beach</b></h5>
+						<a href="https://goo.gl/maps/zbg4Wxm2cTEHPmBh9">
+							<p>North Avenue & Lake Michigan</p>
+						</a>
+						<br />
+						<p>
+							Beach on Lake Michigan woo
+						</p>
+					</div>
+					
+				</div>
+
+				<div class="row">
+					<div class="col m6 s12">
+						<h5 class="teal-text text-darken-4 wedding-date"><b>Alfred Caldwell Lily Pool</b></h5>
+						<a href="https://goo.gl/maps/DmtEZHLm8VTqTyer6">
+							<p>125 W Fullerton Pkwy, Chicago, IL 60614</p>
+						</a>
+						<br />
+						<p>
+							Lily pads yas
+						</p>
+					</div>
+					<div class="col m6 s12">
+						<a href="http://lincolnparkconservancy.org/projects/alfred-caldwell-lily-pool/">
+							<img src="assets/images/things_to_do/alfred-caldwell-lily-pool.jpeg" width="100%" style="border-radius:20px"></img>
+						</a>
+					</div>
+				</div>
+
+				<br />
 
 
 
+
+
+			</div>
 
 		</div>
 	</div>
